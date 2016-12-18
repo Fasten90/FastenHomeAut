35417e9f8c958a71c17fab0df03f0e392cfde9b5
Vizi Gábor <fasten90@gmail.com>
2016-12-18 18:35:14 +0100

    # Readme++

--- README.c
+++ README.c
@@ -42,6 +42,11 @@
  *
  * Common HW functions
  * ------------------------
+ *
+ * First:
+ * If you need help, type on terminal: \code help \endcode
+ * If you can't use a command, type this: \code help <CommandName> \endcode
+ *
  * For common IO-s:
  * - Send IO pin initialization command:
  *    \code	ioinit <port><pin> <input/output> \endcode
@@ -54,25 +59,29 @@
  *
  * For ADC-s (Analog-Digital Converter):
  * - Send command, and you received Analog states (voltages): \code adc \endcode
+ * - If you want periodical ADC reading: \code adcread <milliSec> \endcode
+ *   + Where the <milliSec> is the time of period
  *
  * For DAC-s (Digital-Analog Converter):
- * - Send: \code dac <1/2> <voltage in decimal point> \endcode
+ * - Send: \code dac <1/2> <voltage with decimal point> \endcode
  *   + Example: \code dac 1 3.0 \endcode
- * 1. = A4 pin
- * 2. = A5 pin
+ *   + 1. = A4 pin
+ *   + 2. = A5 pin
  *
  * LED functions:
- * - Command: \code led <set/reset/toggle/status> <1/2/3> \endcode
- *   + Example: \code led set 1 \endcode
+ * - Command: \code led <1/2/3> <on/off/toggle/status> \endcode
+ *   + Example: \code led 1 on \endcode
  *     - 1. LED will set on
  *   + Example: \code led status \endcode
  *     - LED statuses will printed out on Terminal
+ *   + Example: \code led blue off \endcode
+ *     - Blue LED will turn off
  *
  * How development?
  * ------------------------
  *
  * - First, find Vizi Gábor
- * - Check C (embedded) tutorials
+ * - Check C language(embedded) tutorials
  * - Read and check Atollic tutorials
  * - Download or request HomeAut embedded codes
  * - Develop it, write codes
