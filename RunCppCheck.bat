324f08dbeb28c9e11ef39dd238c6c5cfc9d8bd49
Gabor Vizi <fasten90@gmail.com>
2018-09-23 22:37:10 +0200

    # Cppcheck: Beautify bat script + open log file with notepad++

--- RunCppCheck.bat
+++ RunCppCheck.bat
@@ -1,3 +1,5 @@
 echo Run Cppcheck
 
-cppcheck Src Inc Drivers -iDrivers/CMSIS -iDrivers/STM32F0xx_HAL_Driver -iDrivers/STM32F4xx_HAL_Driver --enable=all 2> CppcheckErrors.txt --std=c11 --inconclusive --suppressions-list=cppcheck_suppressions.txt --template="[{file}:{line}]:\t({severity})\t{message}" --inline-suppr
+cppcheck Src Inc Drivers -iDrivers/CMSIS -iDrivers/STM32F0xx_HAL_Driver -iDrivers/STM32F4xx_HAL_Driver --enable=all --inconclusive --suppressions-list=cppcheck_suppressions.txt --template="[{file}:{line}]:\t({severity})\t{message}" --inline-suppr --std=c11 2> Out/CppcheckErrors.txt
+
+start notepad++ "Out/CppcheckErrors.txt"
