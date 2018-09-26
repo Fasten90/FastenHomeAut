53b8cef905877b0866bd43af705527bdc060389d
Gabor Vizi <fasten90@gmail.com>
2018-09-26 23:32:54 +0200

    # CMake: Beautification: Deleted not necessary parts

--- RunCMake.bat
+++ RunCMake.bat
@@ -1,4 +1,5 @@
-::cmake -D"CMAKE_MAKE_PROGRAM:FILEPATH=c:/Programs/Engineer/Utils/CompilerTools/make.exe" -D"CMAKE_C_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/gcc.exe" -D"CMAKE_CXX_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/g++.exe" CC=gcc -G "MinGW Makefiles" ..
+echo Run CMake
+
 
 :: Go to Out/CMakeBuild
 mkdir Out
@@ -6,6 +7,8 @@ cd Out
 mkdir CMakeBuild
 cd CMakeBuild
 
+
+:: Run CMake
 cmake ^
 -D"CMAKE_MAKE_PROGRAM:FILEPATH=c:/Programs/Engineer/Utils/CompilerTools/make.exe" ^
 -D"CMAKE_C_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/gcc.exe" ^
@@ -13,23 +16,15 @@ cmake ^
 CC=gcc -G "MinGW Makefiles" ../..
 
 
-:: CMAKE_C_FLAGS:"-IDrivers/Windows/Inc -IInc/Common -IInc/Communication -IInc/HwTester -IInc/Modules -IInc -IInc/Game -IInc/List -IInc/Display -IInc/Common/Helper -IInc/Common/Handler -IInc/SelfTest -DCONFIG_USE_PANEL_PC=1 -I/Programs/Engineer/LLVM/include" ^
-
-
-
-:: FILEPATH
-
-:: cmake -D"CMAKE_MAKE_PROGRAM:PATH=c:/Programs/Engineer/Utils/CompilerTools/make.exe" -D"CMAKE_C_COMPILER:PATH=c:/Programs/Engineer/mingw64/bin/gcc.exe" -D"CMAKE_CXX_COMPILER:PATH=c:/Programs/Engineer/mingw64/bin/g++.exe" CC=gcc -G "MinGW Makefiles" ..
-
-:: c:\Programs\Engineer\mingw64\bin\gcc.exe
-:: c:\Programs\Engineer\TrueSTUDIO for STM32 9.0.1\PCTools\bin\gcc.exe
-
-:: c:/Programs/Engineer/TrueSTUDIO for STM32 9.0.1/Tools/make.exe
-
-
 :: Run make
 :: c:\Programs\Engineer\Utils\CompilerTools\make.exe
 c:\Programs\Engineer\Utils\CompilerTools\make.exe VERBOSE=1
 
-:: Go to project directory
+
+:: Start compiled exe
 FastenHomeAut.exe
+
+
+:: Go to original directory
+cd ..
+cd ..
