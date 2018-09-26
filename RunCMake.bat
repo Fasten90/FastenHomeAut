5e1e63d1b659dae2e3614d866542d42a4da710b3
Gabor Vizi <fasten90@gmail.com>
2018-09-26 23:25:32 +0200

    # Cmake: Move bat file and change the output directory to "Out/CMakeBuild"

--- RunCMake.bat
+++ RunCMake.bat
@@ -1,11 +1,16 @@
 ::cmake -D"CMAKE_MAKE_PROGRAM:FILEPATH=c:/Programs/Engineer/Utils/CompilerTools/make.exe" -D"CMAKE_C_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/gcc.exe" -D"CMAKE_CXX_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/g++.exe" CC=gcc -G "MinGW Makefiles" ..
 
+:: Go to Out/CMakeBuild
+mkdir Out
+cd Out
+mkdir CMakeBuild
+cd CMakeBuild
 
 cmake ^
 -D"CMAKE_MAKE_PROGRAM:FILEPATH=c:/Programs/Engineer/Utils/CompilerTools/make.exe" ^
 -D"CMAKE_C_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/gcc.exe" ^
 -D"CMAKE_CXX_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/g++.exe" ^
-CC=gcc -G "MinGW Makefiles" ..
+CC=gcc -G "MinGW Makefiles" ../..
 
 
 :: CMAKE_C_FLAGS:"-IDrivers/Windows/Inc -IInc/Common -IInc/Communication -IInc/HwTester -IInc/Modules -IInc -IInc/Game -IInc/List -IInc/Display -IInc/Common/Helper -IInc/Common/Handler -IInc/SelfTest -DCONFIG_USE_PANEL_PC=1 -I/Programs/Engineer/LLVM/include" ^
@@ -25,3 +30,6 @@ CC=gcc -G "MinGW Makefiles" ..
 :: Run make
 :: c:\Programs\Engineer\Utils\CompilerTools\make.exe
 c:\Programs\Engineer\Utils\CompilerTools\make.exe VERBOSE=1
+
+:: Go to project directory
+FastenHomeAut.exe
