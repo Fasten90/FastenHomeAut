cafccf59b10b53351842e29ecf251edb967f6f9f
Gabor Vizi <fasten90@gmail.com>
2018-09-29 00:15:33 +0200

    # CMake: Make 2 different file for MinGW32 and MinGW64

---
+++ RunCMake_Mingw32_atollicPC.bat
@@ -0,0 +1,30 @@
+echo Run CMake
+
+
+:: Go to Out/CMakeBuild
+mkdir Out
+cd Out
+mkdir CMakeBuild
+cd CMakeBuild
+
+
+:: Run CMake
+cmake ^
+-D"CMAKE_MAKE_PROGRAM:FILEPATH=c:/Programs/Engineer/Utils/CompilerTools/make.exe" ^
+-D"CMAKE_C_COMPILER:FILEPATH=c:/Programs/Engineer/TrueSTUDIO for STM32 9.0.1/PCTools/bin/gcc.exe" ^
+-D"CMAKE_CXX_COMPILER:FILEPATH=c:/Programs/Engineer/mingw64/bin/g++.exe" ^
+CC=gcc -G "MinGW Makefiles" ../..
+
+
+:: Run make
+:: c:\Programs\Engineer\Utils\CompilerTools\make.exe
+c:\Programs\Engineer\Utils\CompilerTools\make.exe VERBOSE=1
+
+
+:: Start compiled exe
+FastenHomeAut.exe
+
+
+:: Go to original directory
+cd ..
+cd ..
