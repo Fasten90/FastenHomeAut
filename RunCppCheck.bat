e3b33778f19023a412aa14247c61e7fc056ec7a5
Gabor Vizi <fasten90@gmail.com>
2018-09-29 10:25:55 +0200

    # CppCheck: Added comment (not code run modification)

--- RunCppCheck.bat
+++ RunCppCheck.bat
@@ -2,4 +2,5 @@ echo Run Cppcheck
 
 cppcheck Src Inc Drivers -iDrivers/CMSIS -iDrivers/STM32F0xx_HAL_Driver -iDrivers/STM32F4xx_HAL_Driver --enable=all --inconclusive --suppressions-list=cppcheck_suppressions.txt --template="[{file}:{line}]:\t({severity})\t{message}" --inline-suppr --std=c11 2> Out/CppcheckErrors.txt
 
+:: Open Error file with Notepad++
 start notepad++ "Out/CppcheckErrors.txt"
