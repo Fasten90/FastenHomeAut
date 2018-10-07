ef2a86d76d0d0c59aa7a6c5f9954951d78b97337
Gabor Vizi <fasten90@gmail.com>
2018-10-07 21:58:52 +0200

    # CppCheck: Added force + 2 export: C99, C11

--- RunCppCheck.bat
+++ RunCppCheck.bat
@@ -1,6 +1,11 @@
 echo Run Cppcheck
 
-cppcheck Src Inc Drivers -iDrivers/CMSIS -iDrivers/STM32F0xx_HAL_Driver -iDrivers/STM32F4xx_HAL_Driver --enable=all --inconclusive --suppressions-list=cppcheck_suppressions.txt --template="[{file}:{line}]:\t({severity})\t{message}" --inline-suppr --std=c11 2> Out/CppcheckErrors.txt
+cppcheck Src Inc Drivers -iDrivers/CMSIS -iDrivers/STM32F0xx_HAL_Driver -iDrivers/STM32F4xx_HAL_Driver --enable=all --inconclusive --suppressions-list=cppcheck_suppressions.txt --template="[{file}:{line}]:\t({severity})\t{message}" --inline-suppr --std=c99 --force 2> Out/CppcheckErrorsC99.txt
+
+cppcheck Src Inc Drivers -iDrivers/CMSIS -iDrivers/STM32F0xx_HAL_Driver -iDrivers/STM32F4xx_HAL_Driver --enable=all --inconclusive --suppressions-list=cppcheck_suppressions.txt --template="[{file}:{line}]:\t({severity})\t{message}" --inline-suppr --std=c11 --force 2> Out/CppcheckErrorsC11.txt
 
 :: Open Error file with Notepad++
-start notepad++ "Out/CppcheckErrors.txt"
+start notepad++ "Out/CppcheckErrorsC99.txt"
+
+:: Open Error file with Notepad++
+start notepad++ "Out/CppcheckErrorsC11.txt"
