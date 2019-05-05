8ca7db5595cb2be1719758d6c6f9e6819e586cb4
Gabor Vizi <fasten90@gmail.com>
2019-05-05 22:01:08 +0200

    # CI: Added shell script for linux [Not tested] [skip ci]

---
+++ RunCMake_Mingw64_OnLinux.sh
@@ -0,0 +1,24 @@
+echo Run CMake - MinGW64
+
+# Go to Out/CMakeBuild
+mkdir Out
+cd Out
+mkdir CMakeBuildMinGW64
+cd CMakeBuildMinGW64
+
+
+# Run CMake
+# cmake CC=gcc ..
+cmake ../..
+
+
+# Run make
+make
+
+
+# Start compiled exe
+FastenHomeAut
+
+
+:: Go to original directory
+cd ../..
