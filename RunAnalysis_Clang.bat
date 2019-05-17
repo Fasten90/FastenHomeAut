f4138ba4e794c87b4c2dd2bd95ac68ca55f30c98
Gabor Vizi <fasten90@gmail.com>
2019-05-17 22:42:21 +0200

    # Script: Clang Static analysis on Windows

--- RunAnalysis_Clang.bat
+++ RunAnalysis_Clang.bat
@@ -1,3 +1,17 @@
 :: Analyze - okay
-clang --analyze --analyzer-output html Src/* Src/Common/* Src/Common/Helper/* Src/Common/Handler/* Src/Communication/* Src/HwTester/* Src/Modules/* Src/Application/* Src/List/* Src/Display/* Src/SelfTest/* Drivers/x86/Src/* -IDrivers/x86/Inc -IInc/Common -IInc/Communication -IInc/HwTester -IInc/Modules -IInc -IInc/Application -IInc/List -IInc/Display -IInc/Common/Helper -IInc/Common/Handler -IInc/SelfTest -DCONFIG_USE_PANEL_PC=1 -I/Programs/Engineer/LLVM/include -std=c11 -target x86_64-w64-mingw64 -Wall -S -v
+::clang --analyze --analyzer-output html Src/* Src/Common/* Src/Common/Helper/* Src/Common/Handler/* Src/Communication/* Src/HwTester/* Src/Modules/* Src/Application/* Src/List/* Src/Display/* Src/SelfTest/* Drivers/x86/Src/* -IDrivers/x86/Inc -IInc/Common -IInc/Communication -IInc/HwTester -IInc/Modules -IInc -IInc/Application -IInc/List -IInc/Display -IInc/Common/Helper -IInc/Common/Handler -IInc/SelfTest -DCONFIG_USE_PANEL_PC=1 -I/Programs/Engineer/LLVM/include -std=c11 -target x86_64-w64-mingw64 -Wall -S -v
 
+mkdir Out
+cd Out
+mkdir ClangStaticAnalysisOutput
+cd ClangStaticAnalysisOutput
+cd ../..
+
+clang --analyze -Xanalyzer -analyzer-output=html -v ^
+Src/* Src/Common/* Src/Common/Helper/* Src/Common/Handler/* Src/Communication/* Src/HwTester/* Src/Modules/* Src/Application/* Src/List/* Src/Display/* Src/SelfTest/* Drivers/x86/Src/* ^
+-IDrivers/x86/Inc -IInc/Common -IInc/Communication -IInc/HwTester -IInc/Modules -IInc -IInc/Application -IInc/List -IInc/Display -IInc/Common/Helper -IInc/Common/Handler -IInc/SelfTest ^
+-DCONFIG_USE_PANEL_PC=1 -std=c11 -target x86_64-w64-mingw64 -Wall
+:: -v verbose
+:: -V View in webbrowswer when finished
+:: -o Out/ClangStaticAnalysisOutput  - works at only one file
+:: -stats  - does not works
