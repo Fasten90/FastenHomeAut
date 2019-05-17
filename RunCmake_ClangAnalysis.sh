72b0d5468b9590925a0fa7316bf76bb115faf561
Gabor Vizi <fasten90@gmail.com>
2019-05-17 23:47:05 +0200

    # CI-Azure: Job: Clang Analysis - Linux - try to fix with another mode

--- RunCmake_ClangAnalysis.sh
+++ RunCmake_ClangAnalysis.sh
@@ -16,13 +16,27 @@ export CXX=/usr/bin/clang++
 
 # Run CMake
 # cmake CC=gcc ..
-echo Execute cmake
-cmake ../..
+#echo Execute cmake
+#cmake ../..
 
 
 # Run make
-echo Execute make
-scan-build make
+#echo Execute make
+#scan-build make
+
+
+clang --analyze -Xanalyzer -analyzer-output=html -v -save-stats ^
+../../Src/* ../../Src/Common/* ../../Src/Common/Helper/* ../../Src/Common/Handler/* ^
+../../Src/Communication/* ../../Src/HwTester/* ../../Src/Modules/* ../../Src/Application/* ^
+../../Src/List/* ../../Src/Display/* ../../Src/SelfTest/* ../../Drivers/x86/Src/* ^
+-I../../Drivers/x86/Inc -I../../Inc/Common -I../../Inc/Communication -I../../Inc/HwTester ^
+-I../../Inc/Modules -I../../Inc -I../../Inc/Application -I../../Inc/List -I../../Inc/Display ^
+-I../../Inc/Common/Helper -I../../Inc/Common/Handler -I../../Inc/SelfTest ^
+-DCONFIG_USE_PANEL_PC=1 -std=c11 -target x86_64-pc-linux-gnu -Wall
+
+
+:: List directories
+dir
 
 
 :: Go to original directory
