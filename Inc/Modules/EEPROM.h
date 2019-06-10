879bf9cbd37df67c8aff06b79f64c7daac2c4565
Gabor Vizi <fasten90@gmail.com>
2019-06-10 19:32:37 +0200

    # EEPROM: TODO prepared codes

--- Inc/Modules/EEPROM.h
+++ Inc/Modules/EEPROM.h
@@ -25,12 +25,22 @@
  *  Macros & definitions
  *----------------------------------------------------------------------------*/
 
+#define EEPROM_ADDRESS_START        ((uint16_t)0x0000U)
+#define EEPROM_SIZE                 ((uint16_t)0x800U)
+#define EEPROM_ADDRESS_END          ((uint16_t)(EEPROM_ADDRESS_START + EEPROM_SIZE))
+
 
 
 /*------------------------------------------------------------------------------
  *  Type definitions
  *----------------------------------------------------------------------------*/
 
+typedef enum
+{
+    EEPROM_RESULT_OK,
+    EEPROM_RESULT_ERROR,
+    EEPROM_RESULT_INVALID_ADDRESS,
+} EEPROM_Result_t;
 
 
 /*------------------------------------------------------------------------------
@@ -44,6 +54,9 @@
  *----------------------------------------------------------------------------*/
 
 void EEPROM_Init(void);
+
+EEPROM_Result_t EEPROM_Write(const uint16_t address, const uint8_t * buffer, const uint16_t size);
+EEPROM_Result_t EEPROM_Read(const uint16_t address, uint8_t * buffer, const uint16_t size);
 
 
 
--- Src/Modules/EEPROM.c
+++ Src/Modules/EEPROM.c
@@ -15,9 +15,13 @@
 
 #include "options.h"
 #include "GenericTypeDefs.h"
+#include "compiler.h"
+#include "MemHandler.h"
 #include "EEPROM.h"
 
 
+
+#ifdef CONFIG_MODULE_EEPROM_ENABLE
 
 /*------------------------------------------------------------------------------
  *  Global variables
@@ -51,3 +55,59 @@ void EEPROM_Init(void)
 }
 
 
+
+/**
+ * @brief   Write EEPROM
+ * @param   address EEPROM address, where shall write datas
+ * @param   buffer  Datas, which will be written to the EEPROM
+ * @param   size    size of data in byte
+ */
+EEPROM_Result_t EEPROM_Write(const uint16_t address, const uint8_t * buffer, const uint16_t size)
+{
+    /* Check parameters */
+    ASSERT(buffer != NULL);
+    if (!MEM_IN_FLASH_OR_RAM(buffer, size))
+    {
+        return EEPROM_RESULT_ERROR;
+    }
+
+    if ((address < EEPROM_ADDRESS_START) || ((address + size) > EEPROM_ADDRESS_END))
+    {
+        return EEPROM_RESULT_INVALID_ADDRESS;
+    }
+
+
+    /* TODO: Implement */
+    return EEPROM_RESULT_OK;
+}
+
+
+
+/**
+ * @brief   Read EEPROM
+ * @param   address EEPROM address, where shall read datas
+ * @param   buffer  Datas, which will be written from the EEPROM
+ * @param   size    size of data in byte
+ */
+EEPROM_Result_t EEPROM_Read(const uint16_t address, uint8_t * buffer, const uint16_t size)
+{
+    /* Check parameters */
+    ASSERT(buffer != NULL);
+    if (!MEM_IN_FLASH_OR_RAM(buffer, size))
+    {
+        return EEPROM_RESULT_ERROR;
+    }
+
+    if ((address < EEPROM_ADDRESS_START) || ((address + size) > EEPROM_ADDRESS_END))
+    {
+        return EEPROM_RESULT_INVALID_ADDRESS;
+    }
+
+
+    /* TODO: Implement */
+    return EEPROM_RESULT_OK;
+}
+
+
+
+#endif /* CONFIG_MODULE_EEPROM_ENABLE */
