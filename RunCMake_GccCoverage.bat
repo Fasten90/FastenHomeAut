1581daed51dd73fafbd564cc0c0a612130a288ea
Fasten90 <fasten90@gmail.com>
2019-10-31 00:24:56 +0100

    # GccCoverage:  beautification, comment test codes

--- RunCMake_GccCoverage.bat
+++ RunCMake_GccCoverage.bat
@@ -33,10 +33,10 @@ echo Coverage
 
 :: gcov -abcfu lib.c
 :: --all-blocks                Show information for every basic block
-echo Test commands
-gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/main.c.gcno
-gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/Common/Handler/CommandHandler.c.gcno
-gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/Common/Helper/StringHelper.c.gcno
+::echo Test commands
+::gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/main.c.gcno
+::gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/Common/Handler/CommandHandler.c.gcno
+::gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/Common/Helper/StringHelper.c.gcno
 
 echo Run python script for check all c files
 :: cd ../.. ?
