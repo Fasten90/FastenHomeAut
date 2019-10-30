fb5e70e24da6e42716131f42605643e0c2217330
Fasten90 <fasten90@gmail.com>
2019-10-30 23:54:22 +0100

    # GccCoverage: small improvement in the script

--- Run_GccCoverage_ForSourceFiles.py
+++ Run_GccCoverage_ForSourceFiles.py
@@ -5,41 +5,85 @@ import subprocess
 
 import os
 
+# Note: Similar command shall be called all gcno file
 # gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/main.c.gcno
 
 __COMMAND = "gcov"
 #__COMMAND_ARG = "--all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/"
-__COMMAND_ARG = "CMakeFiles/FastenHomeAut.dir/"
+__gcov_file_dir = "CMakeFiles/FastenHomeAut.dir/"
 
 
 source_list = []
 
-# PATH correction
-os.chdir("../..")
 
+# PATH correction
+
+# os.path.normpath()
+cwd = os.path.normcase(os.path.normpath(os.getcwd()))
+script_dir = os.path.normcase(os.path.normpath(os.path.dirname(__file__)))
+cwd_is_changed = False
+
+if cwd != script_dir:
+    #os.chdir("../..")
+    cwd_is_changed = True
+    os.chdir(script_dir)
+    print("Working directory changed from: '{}' to '{}'".format(cwd, script_dir))
+
+# Source list
 source_list += glob.glob("Src/*.c")
 source_list += glob.glob("Src/**/*.c")
 source_list += glob.glob("Src/**/**/*.c")
 #source_list += Path('Src').glob('**/*.c')
 
+
 # TODO: Shall we append another?
-print("Source list: {}".format(source_list))
+print("Source list:")
+prev_dir = ""
+for src_item in source_list:
+    src_item = src_item.replace("\\\\", "\\").replace("/", "\\")
+    
+    [dir, name] = src_item.rsplit("\\", 1)
+    if dir != prev_dir:
+        prev_dir = dir
+        str_indent = "  " * src_item.count("\\")
+        print(str_indent + "[" + dir + "]")
+    
+    str_indent = "  " * (src_item.count("\\") + 1)
+    print(str_indent + "- " + name)
+
 
 
 # For PATH correction
-os.chdir("Out/CMakeBuild_GccCoverage")
+if cwd_is_changed:
+    os.chdir(cwd)
+    print("Working directory restored to: '{}'".format(cwd))
+else:
+    # TODO: Local mode?
+    os.chdir("Out/CMakeBuild_GccCoverage/")
+    print("Actual working directory: '{}'".format(os.getcwd()))
 
 
 for source in source_list:
     # Call for all source file
-    print(source)
+    #print(source)
+    # TODO: Argument
     source = source.replace("\\", "/")
-    command_arg = __COMMAND_ARG + source + ".gcno"
-    print("Command: {}".format(command_arg))
-    #subprocess.run(["ls", "-l"])
-    #subprocess.run([__COMMAND, command_arg])
-    #return_code = subprocess.call("echo Hello World", shell=True)  
-    return_code = subprocess.call(__COMMAND + " " + command_arg, shell=True)  
+    gcno_file_path = __gcov_file_dir + source + ".gcno"
+    
+    print("file: '{}'".format(gcno_file_path))
+    if os.path.exists(gcno_file_path):
+        # Call
+        command = __COMMAND + " " + gcno_file_path
+        print("  Command: {}".format(command))
+        #subprocess.run(["ls", "-l"])
+        #subprocess.run([__COMMAND, command_arg])
+        #return_code = subprocess.call("echo Hello World", shell=True)  
+        return_code = subprocess.call(command, shell=True)
+        print("  Return code: {}".format(return_code))
+    else:
+        # Do not call
+        print("'{}' has no gcno file".format(source))
+
 
 # from pathlib import Path
     
