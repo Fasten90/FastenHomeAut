99a4495051ff9f501c80440934770f1a240ee209
Fasten90 <fasten90@gmail.com>
2019-11-01 13:01:21 +0100

    # GccCoverage:  Moved to repository

--- RunCMake_GccCoverage.bat
+++ RunCMake_GccCoverage.bat
@@ -25,6 +25,26 @@ FastenHomeAut.exe
 
 echo Coverage
 
+
+:: Clone SCC
+:: PIPELINE_WORKSPACE - Define available at Pipeline (e.g. Azure)
+if defined PIPELINE_WORKSPACE (
+  echo PIPELINE_WORKSPACE is defined, download GccCoverageParser
+  
+  :: Go to original directory
+  cd ../..
+
+  mkdir Tools
+  cd Tools
+ 
+  git clone https://github.com/Fasten90/GccCoverageParser.git
+  
+  cd Out
+  cd CMakeBuild_GccCoverage
+)
+
+
+
 :: TODO: Another files?
 :: gcov ../../Src/main.c
 :: Works:
@@ -38,9 +58,10 @@ echo Coverage
 ::gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/Common/Handler/CommandHandler.c.gcno
 ::gcov --all-blocks --function-summaries --branch-probabilities --branch-counts --unconditional-branches CMakeFiles/FastenHomeAut.dir/Src/Common/Helper/StringHelper.c.gcno
 
-echo Run python script for check all c files
-:: cd ../.. ?
-python ../../Run_GccCoverage_ForSourceFiles.py
+echo Run GccCoverageParser
+
+python ../../Tools/GccCoverageParser/Run_GccCoverage_ForSourceFiles.py
+
 
 
 ::gcc -fprofile-arcs -ftest-coverage main.c
@@ -53,6 +74,6 @@ python ../../Run_GccCoverage_ForSourceFiles.py
 
 
 
-
 :: Go to original directory
 cd ../..
+
