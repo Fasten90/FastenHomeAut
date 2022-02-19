4bdbb9b9dc5d987fd19464bd2219d557c87c53a7
Fasten90 <fasten90@gmail.com>
2022-02-19 19:39:35 +0100

    # Testing GccCoverage and TestSystem changes [TEST]

--- RunCMake_GccCoverage.bat
+++ RunCMake_GccCoverage.bat
@@ -39,6 +39,10 @@ if defined PIPELINE_WORKSPACE (
  
   git clone https://github.com/Fasten90/GccCoverageParser.git
   
+  cd GccCoverageParser
+  git checkout fix_syntax_issue
+  cd ..
+  
   cd ..
 
   cd Out
