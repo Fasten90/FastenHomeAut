2d1d9ef57e85fed160253ef9a0ba551f94cb4c8b
Fasten90 <fasten90@gmail.com>
2022-12-12 23:47:25 +0100

    # Add - FastenCConfigurator - CI Fix - 5.

--- Run_FastenConfigurator.sh
+++ Run_FastenConfigurator.sh
@@ -7,8 +7,15 @@ FASTENCCONFIGURATOR_PATH="FastenCConfigurator"
 mkdir -p Tools
 pushd Tools
 if [[ -d "$FASTENCCONFIGURATOR_PATH" ]]; then
-    pushd $FASTENCCONFIGURATOR_PATH
-    git pull
+    pushd $FASTENCCONFIGURATOR_PATH    
+    # PIPELINE_WORKSPACE - Define available at Pipeline (e.g. Azure)
+    # CI - Predefined variable at Gitlab CI
+    if [[ -z "${PIPELINE_WORKSPACE}" ]] && [[ -z "${CI}" ]] ; then
+        git pull
+    else
+        cp -f ../../../../PytonProjects/FastenCConfigurator/FastenCConfigurator.py .
+        git diff --no-pager || echo "There is no change"
+    fi
     popd
 else
     #git clone git@github.com:Fasten90/FastenCConfigurator.git
