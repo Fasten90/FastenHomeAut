a1d0c9bee39fddc267dcedb7f2b048ba2a3f6cc9
Fasten90 <fasten90@gmail.com>
2022-12-13 00:49:35 +0100

    # Add - FastenCConfigurator - CI Fix - 9.

--- RunCMake_gcc-arm-none-eabi.sh
+++ RunCMake_gcc-arm-none-eabi.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -e
+
+
 echo Run CMake - gcc-arm-none-eabi
 
 
@@ -12,7 +16,7 @@ cmake \
 -G "Unix Makefiles" \
 -D"CMAKE_C_COMPILER:FILEPATH=arm-none-eabi-gcc" \
 -D"CMAKE_CXX_COMPILER:FILEPATH=arm-none-eabi-g++" \
--DCMAKE_TOOLCHAIN_FILE:PATH="../../Toolchain_ST32F0_arm-none-eabi-gcc" \
+-DCMAKE_TOOLCHAIN_FILE:PATH="../../Toolchain_ST32F0_arm-none-eabi-gcc.cmake" \
 ../..
 
 # cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE:PATH="../../toolchain.cmake" ../..
