30c0b86ec3497300f82d1069f35d6f7d78cb9811
Fasten90 <fasten90@gmail.com>
2024-03-16 13:14:37 +0100

    Large font - Simple (not 7segment) numbers

--- Src/Application/SmallApps.c
+++ Src/Application/SmallApps.c
@@ -1111,14 +1111,14 @@ void App_DisplayElevator_Update(ScheduleSource_t source)
 
     char elevator_level_string[6];
 
-    if (App_Elevator_level != 0)
-    {
+    //if (App_Elevator_level != 0)
+    //{
         usnprintf(elevator_level_string, 6, "%d  ", App_Elevator_level);
-    }
-    else
-    {   /* 0 = FSZ */
-        usnprintf(elevator_level_string, 6, "F52  ");
-    }
+    //}
+    //else
+    //{   /* 0 = FSZ */
+    //    usnprintf(elevator_level_string, 6, "F52  ");
+    //}
 
     Display_PrintString(elevator_level_string, 0, Font_32x20, Display_NoFormat);
     Display_Activate();
