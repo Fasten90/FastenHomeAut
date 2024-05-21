4e69c89737afb9ac1a2d0913f8dbae3cdbe44dae
Gabor Vizi <Gabor_Vizi@mentor.com>
2024-05-22 00:09:58 +0200

    Fix Gitlab CI issues - 2.
    FastenStaticAnalyzer has an own install script (which wil lbe wrong for now)
    Try to fix python issue (python3-venv)

--- Run_FastenStaticAnalyzer.sh
+++ Run_FastenStaticAnalyzer.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install
-python3 -m venv venv
-venv/bin/python3 -m pip install -r requirements.txt
-
 
 INCLUDES="-IInc/Common -IInc/Common/Handler -IInc/Common/Helper -IInc/Communication -IInc/HwTester -IInc/Modules -IInc -IInc/Application -IInc/List -IInc/Display -IInc/SelfTest"
 ARGS="-IDrivers/x86/Inc -DCONFIG_USE_PANEL_PC=1"
@@ -16,5 +12,5 @@ DEFAULT_ANALYZER_ARGS="-std=c99 -nostdinc -E -D_Atomic(_arg)=_arg -DINLINE_FUNCT
 # TODO: Not the best pattern for c files
 for file in $(git ls-files -x *c) ; 
 do
-    venv/bin/python3 -u FastenStaticAnalyzer/FastenStaticAnalyzer.py --source=$file --preprocessor="gcc" --preprocessor_args="${DEFAULT_ANALYZER_ARGS} ${ARGS} ${INCLUDES}" --delete_temporary_files --export_file="StaticAnalysisResult.csv" --extend_export_file
+    python3 -u FastenStaticAnalyzer/FastenStaticAnalyzer.py --source=$file --preprocessor="gcc" --preprocessor_args="${DEFAULT_ANALYZER_ARGS} ${ARGS} ${INCLUDES}" --delete_temporary_files --export_file="StaticAnalysisResult.csv" --extend_export_file
 done
